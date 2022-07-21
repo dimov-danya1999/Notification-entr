f452c7062cf52646ee60b36f0cf0a7820ca96af8
dimov-danya1999 <dimov-danya2014@yandex.ru>
2022-07-21 18:36:08 +0300

    Notification

---
+++ FastNotifications-master/FastNotifications/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  FastNotifications
+//
+//  Created by Алексей Пархоменко on 06.04.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let notificationCenter = UNUserNotificationCenter.current()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        notificationCenter.requestAuthorization(options: [.alert,.sound, .badge]) { (granted, error) in
+            
+            guard granted else { return }
+            self.notificationCenter.getNotificationSettings { (settings) in
+                print(settings)
+                guard settings.authorizationStatus == .authorized else { return }
+            }
+        }
+        
+        notificationCenter.delegate = self
+        sendNotifications()
+        return true
+    }
+    
+    func sendNotifications() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "First notification"
+        content.body = "My first notification"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        notificationCenter.add(request) { (error) in
+            print(error?.localizedDescription)
+        }
+    }
+
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+        print(#function)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print(#function)
+    }
+    
+}
+
